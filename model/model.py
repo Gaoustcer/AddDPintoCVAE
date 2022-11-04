dd4b2014a49d5ac194426fdadcfb80cfa416e67e
Gaoustcer <2337284889@qq.com>
2022-11-04 16:38:59 +0800

    clean commit with log

---
+++ model/model.py
@@ -0,0 +1,70 @@
+import torch.nn as nn
+import torch
+import torch.nn.functional as F
+from math import sqrt
+from math import log
+def backwardhook(grad_in:torch.Tensor,C = 0.8,delta = 1e-3,epsilon = 0.01):
+    z = (sqrt(2 * log(1.25/delta)))/epsilon
+    grad_in.clip(-C,C)
+    epsilon_grad = grad_in + z * C * torch.randn_like(grad_in)
+    return epsilon_grad
+    # pass
+class Encoder(nn.Module):
+    def __init__(self,add_noise = False) -> None:
+        super(Encoder,self).__init__()
+        self.flatten = nn.Flatten()
+        self.feature = nn.Sequential(
+            # nn.Flatten(),
+            nn.Linear(28**2 + 10,128),
+            nn.ReLU(),
+            nn.Linear(128,64)
+        )
+        self.muencode = nn.Sequential(
+            nn.Linear(64,32),
+            nn.ReLU(),
+            nn.Linear(32,16)
+        )
+        from copy import deepcopy
+        self.sigmaencode = nn.Sequential(
+            nn.Linear(64,32),
+            nn.ReLU(),
+            nn.Linear(32,16)
+            # nn.ReLU()
+        )
+        if add_noise:
+            for param in self.parameters():
+                param.register_hook(backwardhook)
+    
+    def forward(self,images,labels):
+        images = self.flatten(images)
+        feature = torch.concat([images,labels],-1)
+        feature = self.feature(feature)
+        return self.muencode(feature),self.sigmaencode(feature)
+
+class Decoder(nn.Module):
+    def __init__(self,add_noise=True) -> None:
+        super(Decoder,self).__init__()
+        self.Decode = nn.Sequential(
+            nn.Linear(16 + 10,128),
+            nn.ReLU(),
+            nn.Linear(128,28**2),
+            nn.Sigmoid()
+        )
+        if add_noise:
+            for param in self.parameters():
+                param.register_hook(backwardhook)
+    
+    def forward(self,sigma,mu,labels):
+        noise = torch.randn_like(sigma).cuda()
+        sigma = torch.exp(0.5 * sigma)
+        
+        # output result is log sigma
+        feature = noise * sigma + mu
+        feature = torch.concat([feature,labels],-1)
+        return self.Decode(feature)
+
+
+if __name__ == "__main__":
+    net = Encoder(
+    )
+    net.parameters
---
+++ train.py
@@ -0,0 +1,13 @@
+from ConditionAutoEncoder import CVAE
+import os
+
+if __name__ == "__main__":
+    Condition_Variation_AutoEncoder = CVAE(add_noise=False)
+    EPOCH = 32
+    rootpath = "picture"
+    if os.path.exists(rootpath) == False:
+        os.mkdir(rootpath)
+    for epoch in range(EPOCH):
+        Condition_Variation_AutoEncoder.train()
+        Condition_Variation_AutoEncoder.deduction(rootpath+"/deduction{}".format(epoch))
+        
